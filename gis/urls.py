afaf397d2a38479506282664dd37ff9c91721355
arissaputra <laodearissaputra94@gmail.com>
2020-08-11 17:24:42 +0700

    add dashboard & menu owner, parent, employee and parent

--- gis/urls.py
+++ gis/urls.py
@@ -2,13 +2,43 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    # employee
     path('', views.index, name='home'),
     path('employee', views.employee, name='employee'),
+    path('card', views.card, name='card'),
+    path('attendance/employee', views.attendanceEmployee, name='attendance_employee'),
+    path('attendance/student', views.attendanceStudent, name='attendance_student'),
+    path('book-parental', views.bookParental, name='book_parental'),
     path('calender', views.calender, name='calender'),
     path('schedule', views.schedule, name='schedule'),
     path('learning', views.learning, name='learning'),
     path('raport', views.raport, name='raport'),
     path('exam', views.exam, name='exam'),
+
+    # Student
+    path('student', views.student, name='student'),
+    path('student/calender', views.studentCalender, name='student_calender'),
+    path('student/schedule', views.studentSchedule, name='student_schedule'),
+    path('student/card', views.studentCard, name='student_card'),
+    path('student/learning', views.studentLearning, name='student_learning'),
+    path('student/raport', views.studentRaport, name='student_raport'),
+    path('student/exam', views.studentExam, name='student_exam'),
+
+    # Parent
+    path('parent', views.parent, name='parent'),
+    path('parent/parent-calender', views.calenderParent, name='parent_calender'),
+    path('parent/parent-schedule', views.scheduleParent, name='parent_schedule'),
+    path('parent/attendance-student', views.parentAttendanceStudent, name='parent_attendance_student'),
+    path('parent/parent-card', views.Parentcard, name='parent_card'),
+    path('parent/parent-raport', views.parentRaport, name='parent_raport'),
+
+    # Owner
+    path('owner', views.owner, name='owner'),
+    path('owner/owner-calendar', views.ownerCalendar, name='owner_calendar'),
+    path('owner/owner-attendance', views.ownerAttendance, name='owner_attendance'),
+    path('owner/owner-payment', views.ownerPayment, name='owner_payment'),
+
+
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
 ]
--- gis/views.py
+++ gis/views.py
@@ -2,26 +2,68 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import LoginForm
 
+# Employee
 def index(request):
-    return render(request, 'index.html')
-
+    return render(request, 'employee/index.html')
 def employee(request):
-    return render(request, 'employee.html')
-
+    return render(request, 'employee/employee.html')
+def card(request):
+    return render(request, 'employee/card.html')
+def attendanceStudent(request):
+    return render(request, 'employee/attendance_student.html')
+def attendanceEmployee(request):
+    return render(request, 'employee/attendance_employee.html')
 def calender(request):
-    return render(request, 'calender.html')
-
+    return render(request, 'employee/calender.html')
 def schedule(request):
-    return render(request, 'schedule.html')
-
+    return render(request, 'employee/schedule.html')
 def learning(request):
-    return render(request, 'learning.html')
-
+    return render(request, 'employee/learning.html')
 def raport(request):
-    return render(request, 'raport.html')
-
+    return render(request, 'employee/raport.html')
 def exam(request):
-    return render(request, 'exam.html')
+    return render(request, 'employee/exam.html')
+
+# Student
+def student(request):
+    return render(request, 'student/student.html')
+def studentCard(request):
+    return render(request, 'student/student_card.html')
+def studentCalender(request):
+    return render(request, 'student/student_calender.html')
+def studentSchedule(request):
+    return render(request, 'student/student_schedule.html')
+def studentLearning(request):
+    return render(request, 'student/student_learning.html')
+def studentRaport(request):
+    return render(request, 'student/student_raport.html')
+def studentExam(request):
+    return render(request, 'student/student_exam.html')
+
+# Parent
+def parent(request):
+    return render(request, 'parent/parent.html')
+def calenderParent(request):
+    return render(request, 'parent/calender_parent.html')
+def scheduleParent(request):
+    return render(request, 'parent/schedule_parent.html')
+def parentAttendanceStudent(request):
+    return render(request, 'parent/parent_attendance_student.html')
+def Parentcard(request):
+    return render(request, 'parent/parent_card.html')
+def parentRaport(request):
+    return render(request, 'parent/parent_raport.html')
+def bookParental(request):
+    return render(request, 'parent/book_parental.html')
+
+def owner(request):
+    return render(request, 'owner/owner.html')
+def ownerAttendance(request):
+    return render(request, 'owner/attendance.html')
+def ownerCalendar(request):
+    return render(request, 'owner/owner_calender.html')
+def ownerPayment(request):
+    return render(request, 'owner/payment.html')
 
 def login(request):
     if request.method == 'POST':
